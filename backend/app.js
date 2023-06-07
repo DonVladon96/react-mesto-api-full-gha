c2e608a0198ca7f10ca70bac8b40d45312596593
DonVladon96 <Petrov.Vladik2010@yandex.com>
2023-06-07 16:36:43 +0300

    Добавляем пакет cors

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const router = require('./routes');
 const errorMiddlewares = require('./middlewares/errorMiddlewares');
@@ -13,6 +14,7 @@ const PORT = 3000;
 app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb').then(() => {
   console.log('Connected to database.');
